b3b55bf6471225594dcdf99dd7cdae05c7f0ea50
921-Andreea-Ciama <91825329+911-Andreea-Ciama@users.noreply.github.com>
2023-11-01 17:50:41 +0200

    Update main.py

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from symbolTable import HashTable
+from pif import PIF
+from scanner import Scanner, is_identifier, is_constant
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ == "__main__":
+    ST = HashTable(10)
+    pif = PIF()
+    scanner = Scanner()
 
+    program = "p1.txt"
+    exception = ""
+    scanner.read_tokens()
+    #print(scanner.get_all())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    with open(program, 'r') as f:
+        line = f.readline()
+        index_line = 1
+        for line in f:
+            tokens = scanner.get_line_tokens(line.strip())
+            for i in range(len(tokens)):
+                if tokens[i] in scanner.get_all():
+                    if tokens[i] == ' ':
+                        continue
+                    pif.insert(tokens[i], (-1, -1))
+                elif is_identifier(tokens[i]):
+                    ST.add(tokens[i])
+                    identifier = tokens[i]
+                    pif.insert("id", ST.getIndex(identifier))
+                elif is_constant(tokens[i]):
+                    ST.add(tokens[i])
+                    constant = tokens[i]
+                    pif.insert("const", ST.getIndex(constant))
+                else:
+                    exception += 'Lexical error --> ' + tokens[i] + ' - at line ' + str(index_line+1) + "\n"
+            index_line += 1
 
+    with open('PIF.out', 'w') as writer:
+        writer.write(str(pif))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('ST.out', 'w') as writer:
+        writer.write(str(ST))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if exception == '':
+        print("Lexically correct!")
+    else:
+        print(exception)
